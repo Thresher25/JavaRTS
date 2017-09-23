17f8f156c87dbd6206f973aad06f6c51c614a118
Thresher25 <12576332+Thresher25@users.noreply.github.com>
2017-09-22 23:09:50 -0400

    Added a JFrame Window

--- JavaRTS/src/main/MainClass.java
+++ JavaRTS/src/main/MainClass.java
@@ -2,11 +2,36 @@ package main;
 
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainClass extends JPanel {
 
+    public static final int SCREENWIDTH = 1920;
+    public static final int SCREENHEIGHT = 1080;
+
+    public MainClass() {
+        this.setSize(SCREENWIDTH, SCREENHEIGHT);
+        this.setVisible(true);
+        this.setDoubleBuffered(true);
+
+        JFrame frame = new JFrame("JavaRTS");
+        frame.setSize(SCREENWIDTH, SCREENHEIGHT);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.add(this);
+        frame.repaint();
+    }
+
     public static void main(String[] args){
-        
+        MainClass mc = new MainClass();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.clearRect(0, 0, SCREENWIDTH, SCREENHEIGHT);
+        g.setColor(Color.MAGENTA);
+        g.fillRect(0, 0, SCREENWIDTH, SCREENHEIGHT);
     }
 
 
