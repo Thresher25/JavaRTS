150837a0019841bf7a25f4453bcaff74a3a31bfb
Springroll <12576332+Thresher25@users.noreply.github.com>
2017-09-24 18:22:14 -0400

    Added/Fixed Game loop

--- JavaRTS/src/main/MainClass.java
+++ JavaRTS/src/main/MainClass.java
@@ -8,6 +8,7 @@ public class MainClass extends JPanel {
 
     public static final int SCREENWIDTH = 1920;
     public static final int SCREENHEIGHT = 1080;
+    boolean quit = false;
     public JFrame frame;
 
     public MainClass() {
@@ -25,7 +26,34 @@ public class MainClass extends JPanel {
 
     public static void main(String[] args){
         MainClass mc = new MainClass();
-        mc.frame.repaint();
+        double previousTime = System.currentTimeMillis();
+        double currentTime = System.currentTimeMillis();
+        double deltaTime = 0;
+        double maxDelta = 1 / 60;
+        double minDelta = 1 / 120;
+        while (!mc.quit) {
+            currentTime = System.currentTimeMillis();
+            deltaTime = currentTime - previousTime;
+            previousTime = System.currentTimeMillis();
+            while (deltaTime < minDelta) {
+                mc.frame.repaint();
+                deltaTime += System.currentTimeMillis() - previousTime;
+            }
+            if (deltaTime <= maxDelta) {
+                mc.update(deltaTime);
+            } else if (deltaTime > maxDelta) {
+                int div = (int) deltaTime / (int) maxDelta;
+                for (int i = 0; i < div; i++) {
+                    mc.update(deltaTime / (double) div);
+                }
+            }
+            mc.frame.repaint();
+        }
+
+    }
+
+    public void update(double pTimeElapsed) {
+
     }
 
     @Override
