f2f8a32170323f2f4c55726852cc36d44791a4b9
Springroll <12576332+Thresher25@users.noreply.github.com>
2017-09-24 20:35:39 -0400

    Created Tile Class

---
+++ JavaRTS/src/Tiles/Tile.java
@@ -0,0 +1,13 @@
+package Tiles;
+
+import java.awt.image.BufferedImage;
+
+public class Tile {
+    public BufferedImage image;
+    public int xPos, yPos;
+
+    public Tile(String pName) {
+        
+    }
+
+}
