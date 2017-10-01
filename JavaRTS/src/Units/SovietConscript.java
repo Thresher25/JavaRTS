34db33cf13eb66ca95852fc0b7ac4b6a6d6b05b0
Springroll <12576332+Thresher25@users.noreply.github.com>
2017-09-30 23:48:07 -0400

    Started work on units

---
+++ JavaRTS/src/Units/SovietConscript.java
@@ -0,0 +1,15 @@
+package Units;
+
+import java.awt.image.BufferedImage;
+
+public class SovietConscript {
+
+    public static BufferedImage image;
+    public int state;
+
+    public SovietConscript() {
+
+    }
+
+
+}
