277071784724f5e4cb818d3dddb89cc0d939d2c5
Thresher25 <12576332+Thresher25@users.noreply.github.com>
2017-10-24 15:51:48 -0400

    Working on flipping unit horizontally

--- JavaRTS/src/Units/SovietConscript.java
+++ JavaRTS/src/Units/SovietConscript.java
@@ -2,6 +2,8 @@ package Units;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -11,13 +13,15 @@ public class SovietConscript {
     private static BufferedImage[][] image = new BufferedImage[10][8];
     private int state, curImageX, curImageY, count;
     //Possibly add either an AI state, or add an AI class to each unit
+    private AffineTransform flip = AffineTransform.getScaleInstance(-1, 1);
+    AffineTransformOp op = new AffineTransformOp(flip,AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
     private float xPos, yPos, angle;
     private double timePassed;
     private boolean movingRight = true;
     private static String filePath = "res/CivSprite.png";
 
     public SovietConscript(float x, float y) {
-
+        
         try {
             BufferedImage temp = ImageIO.read(new File(filePath));
             for (int i = 0; i < image.length; i++) {
@@ -53,6 +57,7 @@ public class SovietConscript {
                 curImageY = 0;
                 if (curImageX >= 9) {
                     curImageX = 0;
+                    movingRight = !movingRight;
                 } else {
                     curImageX++;
                 }
@@ -67,10 +72,21 @@ public class SovietConscript {
     }
 
     public void draw(Graphics g) {
-
-        g.drawImage(image[curImageX][curImageY], (int) xPos - image[curImageX][curImageY].getWidth() / 2, (int) yPos - image[curImageX][curImageY].getHeight() / 2, null);
-
+        if(movingRight){
+            AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
+            tx.translate(-image[curImageX][curImageY].getWidth(null), 0);
+             op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+            image[curImageX][curImageY] = op.filter(image[curImageX][curImageY], null);
+           
+            g.drawImage(image[curImageX][curImageY], (int) xPos - image[curImageX][curImageY].getWidth() / 2, (int) yPos - image[curImageX][curImageY].getHeight() / 2, null);
+        image[curImageX][curImageY] = op.filter(image[curImageX][curImageY], null);
+        }else{
+            
+            g.drawImage(image[curImageX][curImageY], (int) xPos - image[curImageX][curImageY].getWidth() / 2, (int) yPos - image[curImageX][curImageY].getHeight() / 2, null);
+        }
+            
     }
+    
     public int getCurImageX(){
         return curImageX;
     }
