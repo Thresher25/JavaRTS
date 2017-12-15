a34f8d34995a8c9b8a0c7a18078cd65800400a94
Thresher25 <12576332+Thresher25@users.noreply.github.com>
2017-12-15 10:38:28 -0500

    Added proper image staging and update method

--- JavaRTS/src/Structures/Structure.java
+++ JavaRTS/src/Structures/Structure.java
@@ -3,13 +3,29 @@ package Structures;
 import main.GameObject;
 
 public abstract class Structure extends GameObject {
-
+    protected int stages;
+    protected float hpPerStage;
+    protected boolean finishedConstruction;
+    protected int curImage = 0;
     public Structure() {
         super();
     }
 
     public Structure(double x, double y) {
         super(x, y);
+    }
+    
+    @Override
+    public void update(double time){
+        HP++;
+        if(HP==hpPerStage*stages){
+            finishedConstruction = true;
+        }
+        if(finishedConstruction){
+            curImage = stages;
+        }else{
+            curImage = (int)(HP/hpPerStage);
+        }
     }
 
 }
